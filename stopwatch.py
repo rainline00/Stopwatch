274a8383d2dd665db024e78fc09c3f3569a152c1
rainline00 <kanjyukaggle@gmail.com>
2019-12-09 04:12:15 +0900

    prefect

--- stopwatch.py
+++ stopwatch.py
@@ -10,6 +10,47 @@ class Application(tk.Frame):
         master.title("Stopwatch")
         master.config(bg="black")
 
+        self.startTime = time.time()
+        self.stopTime = 0.0
+        self.elapsedTime = 0.0
+        self.playTime = False
+
+        self.canvas = tk.Canvas(master,width=290,height=80,bg="skyblue")
+        self.canvas.place(x=3,y=10)
+
+        tk.Button(master,text="リセット",command=self.resetButtonClick,width=10).place(x=10, y=110)
+        tk.Button(master,text="スタート",command=self.startButtonClick,width=10).place(x=110, y=110)
+        tk.Button(master,text="ストップ",command=self.stopButtonClick,width=10).place(x=210, y=110)
+
+        master.after(50, self.update)
+
+    def startButtonClick(self):
+        if not self.playTime:
+            self.startTime=time.time()-self.elapsedTime
+            self.playTime=True
+
+    def stopButtonClick(self):
+        if self.playTime:
+            self.stopTime=time.time()-self.startTime
+            self.playTime=False
+
+    def resetButtonClick(self):
+        self.startTime=time.time()
+        self.stopTime=0.0
+        self.elapsedTime=0.0
+        self.playTime=False
+
+    def update(self):
+        self.canvas.delete("Time")
+        if self.playTime:
+            self.elapsedTime=time.time()-self.startTime
+            self.canvas.create_text(280,40,text=round(self.elapsedTime,2),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
+        else:
+            self.canvas.create_text(280,40,text=round(self.stopTime,2),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
+
+        self.master.after(50,self.update)
+
+
 def main():
     win = tk.Tk()
     app = Application(master=win)
